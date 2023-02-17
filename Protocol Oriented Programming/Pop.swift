5184a6079616f882f7338868a69e0fec51460231
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-17 16:29:01 +0000

    Protocol Oriented Programming

--- Protocol Oriented Programming/Pop.swift
+++ Protocol Oriented Programming/Pop.swift
@@ -17,5 +17,98 @@ class TelephoneClass {
 }
 
 class LandlineClass: TelephoneClass {
-    <#code#>
+    
+}
+
+protocol Telephone {
+    var phoneNo: String { get set}
+    func call()
+    func handUp()
+}
+extension Telephone {
+    func call(){
+        
+    }
+    func handUp(){
+        
+    }
+}
+
+
+protocol Landline: Telephone {
+    
+}
+protocol Cellular: Telephone {
+    func sendSMS()
+}
+
+protocol Rotaryable {
+    func rotaryInput()
+}
+protocol PushButtonable {
+    func buttonInput()
+}
+protocol Touchable {
+    func touchInput()
+}
+
+
+class RotaryPhone: Landline, Rotaryable {
+    func rotaryInput() {
+        
+    }
+    
+    var phoneNo: String
+    
+    init(phoneNo: String){
+        self.phoneNo = phoneNo
+    }
+    
+}
+
+class PushButtonPhone: Landline, PushButtonable {
+    func buttonInput() {
+        <#code#>
+    }
+    
+    var phoneNo: String
+    
+    init(phoneNo: String){
+        self.phoneNo = phoneNo
+    }
+    
+}
+
+class NonSmartPhone: Cellular, PushButtonable {
+    func sendSMS() {
+        <#code#>
+    }
+    
+    func buttonInput() {
+        <#code#>
+    }
+    
+    var phoneNo: String
+    
+    init(phoneNo: String){
+        self.phoneNo = phoneNo
+    }
+    
+}
+
+class smartPhone: Cellular, Touchable {
+    func touchInput() {
+        <#code#>
+    }
+    
+    func sendSMS() {
+        <#code#>
+    }
+    
+     var phoneNo: String
+    
+    init(phoneNo: String){
+        self.phoneNo = phoneNo
+    }
+    
 }
