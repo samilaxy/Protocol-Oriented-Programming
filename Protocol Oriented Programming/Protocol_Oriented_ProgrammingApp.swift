32d9aa1262c8ef77414195033cc83e5bf96465a8
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-17 15:36:11 +0000

    Initial Commit

---
+++ Protocol Oriented Programming/Protocol_Oriented_ProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Protocol_Oriented_ProgrammingApp.swift
+//  Protocol Oriented Programming
+//
+//  Created by Noye Samuel on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Protocol_Oriented_ProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
